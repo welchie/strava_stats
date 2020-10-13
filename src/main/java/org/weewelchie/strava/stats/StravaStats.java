344c686d4e132333932c7fb448de6a8b150f42df
welchie <chris@weewelchie.org>
2020-10-13 14:01:36 +0100

    Updated to use javastrav3api-2.0.1-SNAPSHOT

--- src/main/java/org/weewelchie/strava/stats/StravaStats.java
+++ src/main/java/org/weewelchie/strava/stats/StravaStats.java
@@ -1,25 +1,28 @@
 package org.weewelchie.strava.stats;
 
-import io.swagger.models.AuthorizationScope;
-import javastrava.api.v3.auth.AuthorisationService;
-import javastrava.api.v3.auth.impl.retrofit.AuthorisationServiceImpl;
-import javastrava.api.v3.auth.model.Token;
-import javastrava.api.v3.auth.ref.AuthorisationScope;
-import javastrava.api.v3.model.StravaAthlete;
-import javastrava.api.v3.service.Strava;
+import javastrava.auth.AuthorisationService;
+import javastrava.auth.impl.AuthorisationServiceImpl;
+import javastrava.auth.model.Token;
+import javastrava.auth.ref.AuthorisationScope;
+import javastrava.model.StravaAthlete;
+import javastrava.service.Strava;
 
 public class StravaStats {
 
 	private static final String ACCESS_TOKEN = "4b2c7d8dc012a8d5af862949f1483dd783e10460";
 	private static final Integer CLIENT_ID = 48445;
 	private static final String CLIENT_SECRET = "d906ca7e1a4cecc32a33714266d5baeaa6918e75";
-	private static final String CODE = "ad2ee73738e4b1e88c1132dcefb9701b3505b7b5";
+	private static final String CODE = "415d6bc9e935a7a92fe7e1e329eee7306d20dc4b";
 	private static final Integer ATHLETE_ID = 13986969;
 	
 	public static void main(String[] args) {
 		
 		AuthorisationService service = new AuthorisationServiceImpl();
 		AuthorisationScope scope = AuthorisationScope.UNKNOWN;
+		
+		//Call Strava API with CLIENT ID to retrieve CODE
+		//https://www.strava.com/oauth/authorize?client_id=48445&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read,activity:read
+		//CURL command and then redirect?
 		
 		Token token = service.tokenExchange(CLIENT_ID, CLIENT_SECRET, CODE);
 		
