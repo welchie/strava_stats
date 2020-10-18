ba014e81c41e2e40e753be62aacd6f972a8acb85
welchie <chris@weewelchie.org>
2020-10-18 01:30:01 +0100

    Getting data from my Strav App

--- src/main/java/org/weewelchie/strava/stats/StravaActivities.java
+++ src/main/java/org/weewelchie/strava/stats/StravaActivities.java
@@ -1,98 +1,167 @@
 package org.weewelchie.strava.stats;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Observable;
-
-import io.swagger.client.ApiClient;
-import io.swagger.client.ApiException;
-import io.swagger.client.Configuration;
-import io.swagger.client.Pair;
-import io.swagger.client.api.AthletesApi;
-import io.swagger.client.auth.OAuth;
-import io.swagger.client.model.ActivityStats;
-import io.swagger.client.model.DetailedActivity;
-import io.swagger.client.model.DetailedAthlete;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class StravaActivities {
 
-	private static final String ACCESS_TOKEN = "4b2c7d8dc012a8d5af862949f1483dd783e10460";
-	private static final String CLIENT_ID = "48445";
 	
 	
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-
-		ApiClient defaultClient = Configuration.getDefaultApiClient();
-
-		// Configure OAuth2 access token for authorization: strava_oauth
-		//defaultClient.setApiKey(CLIENT_ID);
-		OAuth strava_oauth = (OAuth) defaultClient.getAuthentication("strava_oauth");
-		strava_oauth.setAccessToken(ACCESS_TOKEN);
-		
-		List<Pair> queryParams = new ArrayList<Pair>();
-		queryParams.add(new Pair("client_id", CLIENT_ID));
-		queryParams.add(new Pair("redirect_uri", "https://www.strava.com/oauth/authorize"));
-		queryParams.add(new Pair("response_type", "code"));
-		queryParams.add(new Pair("approval_prompt", "auto"));
-		queryParams.add(new Pair("scope", "activity:write,read"));
 		
 		
-		Map<String, String> headerParams = new HashMap<String, String>();
-		headerParams.put("Authorization", "Bearer " + ACCESS_TOKEN);
-		strava_oauth.applyToParams(queryParams, headerParams);
-		strava_oauth = (OAuth) defaultClient.getAuthentication("strava_oauth");
+		//Test JSON Array Here
+		//String jsonArr = new String("[{\"name\": \"One\"},{\"name\":\"two\"},{\"name\":\"three\"}]");
 		
+		String jsonArr = new String("[{\r\n"
+				+ "		\"resource_state\": 2,\r\n"
+				+ "		\"athlete\": {\r\n"
+				+ "			\"id\": 13986969,\r\n"
+				+ "			\"resource_state\": 1\r\n"
+				+ "		},\r\n"
+				+ "		\"name\": \"Lunch Run Full Monty without the hills.\",\r\n"
+				+ "		\"distance\": 10218.6,\r\n"
+				+ "		\"moving_time\": 3070,\r\n"
+				+ "		\"elapsed_time\": 3070,\r\n"
+				+ "		\"total_elevation_gain\": 111.1,\r\n"
+				+ "		\"type\": \"Run\",\r\n"
+				+ "		\"workout_type\": 0,\r\n"
+				+ "		\"id\": 4201988109,\r\n"
+				+ "		\"external_id\": \"garmin_push_5684681896\",\r\n"
+				+ "		\"upload_id\": 4495631038,\r\n"
+				+ "		\"start_date\": \"2020-10-16T11:24:04Z\",\r\n"
+				+ "		\"start_date_local\": \"2020-10-16T12:24:04Z\",\r\n"
+				+ "		\"timezone\": \"(GMT+00:00) Europe/London\",\r\n"
+				+ "		\"utc_offset\": 3600.0,\r\n"
+				+ "		\"start_latlng\": [\r\n"
+				+ "			56.04,\r\n"
+				+ "			-3.37\r\n"
+				+ "		],\r\n"
+				+ "		\"end_latlng\": [\r\n"
+				+ "			56.04,\r\n"
+				+ "			-3.37\r\n"
+				+ "		],\r\n"
+				+ "		\"location_city\": null,\r\n"
+				+ "		\"location_state\": null,\r\n"
+				+ "		\"location_country\": \"United Kingdom\",\r\n"
+				+ "		\"start_latitude\": 56.04,\r\n"
+				+ "		\"start_longitude\": -3.37,\r\n"
+				+ "		\"achievement_count\": 1,\r\n"
+				+ "		\"kudos_count\": 3,\r\n"
+				+ "		\"comment_count\": 0,\r\n"
+				+ "		\"athlete_count\": 1,\r\n"
+				+ "		\"photo_count\": 0,\r\n"
+				+ "		\"map\": {\r\n"
+				+ "			\"id\": \"a4201988109\",\r\n"
+				+ "			\"summary_polyline\": \"wm_uIlfqSABI@[OMK}BmC}@_AYi@EeAHq@N_@D[EO]i@_@SQcAGiAMsAIe@Wu@e@m@c@a@w@g@_@a@c@?c@Gg@D_@_@OeAS}@gAgDMcACkADiAZkCDiAEgAO}@S}@K{@PaAzBaEVo@R}@DcAAcA_AoDQ{@K_AQ{@IeA[_C}AqLO_Aw@sGSsDIqEo@iTGiAg@mGOmCGaCUiC]_CSaASaCa@}B]yBOgCGgAH}@Zy@v@uAVu@G_A[_@e@@e@HMCSaCDiC@gACgAQkCGeEBgAAaAMiEM_C?iCEcAM{HMaFAiADgAn@yIBeAHaABeAE_AQcAg@{BE_@hBkAd@UdAo@h@UhBcA^KF|@C|@Xn@b@TL~@ChCKvBGhBDdARnCZbCR`CPtCFnCCdAMbAWx@Ox@K|@GbA?fALfCR`C@`ACdAShCOdC?~@B`AH`A`@|BZ\\\\LbALlF@dAC~@FbA\\\\`CPv@P~@J`A\\\\|Hn@`ER~@b@zCTx@XtD^xBd@bB^vBPx@j@dB^f@f@Fb@RXn@JdADbALz@Zj@`@Yd@a@bAyA^m@n@kBA_AEcAp@uFLuDP{BZg@d@GlAHZm@RcAT{@PQ\\\\f@P~@\\\\^b@]`@\\\\Zl@HhAF~BP|@VdA^bCd@vAVn@x@rAlAXd@@h@C`@NPv@CbAUhBKdEFtAN`AThADdAM~CbA|@Vr@d@Td@Ch@H^b@Xl@Vt@b@`@f@NnCnAhA]Zf@Nv@JtAb@vBZl@^\\\\XxAd@bED`AI~@AbAThAX`CD`ABvAK`AIdAWn@OVGfGJdAHf@\\\\`B^rBNhCCdAMdAWn@{@zAm@pAo@z@AfAQ`AQv@Wt@cAjAc@Lg@FeBdB]f@OJg@To@DSLCz@a@\\\\]BgAVWP_@f@[p@_BxE[f@c@\\\\e@Le@Be@AkAIg@Oa@Ui@CmAWeAe@c@Y_@c@_@o@_AgC]iAw@gB_AoCs@wA]e@}AcB_@[aByB_A_As@a@q@OUNi@`@?DlAzC`@lAZd@^\\\\`@f@Tx@NfAp@n@F~@I~@Ax@Vt@^l@b@h@bCzB\",\r\n"
+				+ "			\"resource_state\": 2\r\n"
+				+ "		},\r\n"
+				+ "		\"trainer\": false,\r\n"
+				+ "		\"commute\": false,\r\n"
+				+ "		\"manual\": false,\r\n"
+				+ "		\"private\": false,\r\n"
+				+ "		\"visibility\": \"everyone\",\r\n"
+				+ "		\"flagged\": false,\r\n"
+				+ "		\"gear_id\": \"g6382672\",\r\n"
+				+ "		\"from_accepted_tag\": false,\r\n"
+				+ "		\"upload_id_str\": \"4495631038\",\r\n"
+				+ "		\"average_speed\": 3.329,\r\n"
+				+ "		\"max_speed\": 4.4,\r\n"
+				+ "		\"average_cadence\": 92.7,\r\n"
+				+ "		\"has_heartrate\": true,\r\n"
+				+ "		\"average_heartrate\": 134.4,\r\n"
+				+ "		\"max_heartrate\": 145.0,\r\n"
+				+ "		\"heartrate_opt_out\": false,\r\n"
+				+ "		\"display_hide_heartrate_option\": true,\r\n"
+				+ "		\"elev_high\": 55.1,\r\n"
+				+ "		\"elev_low\": 6.5,\r\n"
+				+ "		\"pr_count\": 0,\r\n"
+				+ "		\"total_photo_count\": 0,\r\n"
+				+ "		\"has_kudoed\": false\r\n"
+				+ "	},\r\n"
+				+ "	{\r\n"
+				+ "		\"resource_state\": 2,\r\n"
+				+ "		\"athlete\": {\r\n"
+				+ "			\"id\": 13986969,\r\n"
+				+ "			\"resource_state\": 1\r\n"
+				+ "		},\r\n"
+				+ "		\"name\": \"Lunch Run coastal path\",\r\n"
+				+ "		\"distance\": 5194.8,\r\n"
+				+ "		\"moving_time\": 1566,\r\n"
+				+ "		\"elapsed_time\": 1566,\r\n"
+				+ "		\"total_elevation_gain\": 73.5,\r\n"
+				+ "		\"type\": \"Run\",\r\n"
+				+ "		\"workout_type\": 0,\r\n"
+				+ "		\"id\": 4193450794,\r\n"
+				+ "		\"external_id\": \"garmin_push_5675793003\",\r\n"
+				+ "		\"upload_id\": 4486564059,\r\n"
+				+ "		\"start_date\": \"2020-10-14T11:22:36Z\",\r\n"
+				+ "		\"start_date_local\": \"2020-10-14T12:22:36Z\",\r\n"
+				+ "		\"timezone\": \"(GMT+00:00) Europe/London\",\r\n"
+				+ "		\"utc_offset\": 3600.0,\r\n"
+				+ "		\"start_latlng\": [\r\n"
+				+ "			56.04,\r\n"
+				+ "			-3.37\r\n"
+				+ "		],\r\n"
+				+ "		\"end_latlng\": [\r\n"
+				+ "			56.04,\r\n"
+				+ "			-3.37\r\n"
+				+ "		],\r\n"
+				+ "		\"location_city\": null,\r\n"
+				+ "		\"location_state\": null,\r\n"
+				+ "		\"location_country\": \"United Kingdom\",\r\n"
+				+ "		\"start_latitude\": 56.04,\r\n"
+				+ "		\"start_longitude\": -3.37,\r\n"
+				+ "		\"achievement_count\": 0,\r\n"
+				+ "		\"kudos_count\": 2,\r\n"
+				+ "		\"comment_count\": 0,\r\n"
+				+ "		\"athlete_count\": 1,\r\n"
+				+ "		\"photo_count\": 0,\r\n"
+				+ "		\"map\": {\r\n"
+				+ "			\"id\": \"a4193450794\",\r\n"
+				+ "			\"summary_polyline\": \"op_uIfdqSu@cA_@_@a@Yk@o@Wq@Ay@VuA?QIYq@s@M}@YaECQWu@q@_AwAcAe@Se@Lc@Ka@UWs@c@cBo@gBWy@MgA?kAB_ATkCBgAIoAM_AS{@Gq@\\\\m@`@k@Zm@lAqC^k@jA_@Py@CeA@eAj@wBXw@x@aBn@{AxBuD\\\\g@bCiE~@sA~@{Ab@Yz@MXq@@I?eACk@Jy@h@}BJ_AHgAVeJNgCx@qIAcAGeAYeCP{@`@Yd@If@Tb@h@^n@hAlEr@hB\\\\`@t@b@`@^z@pARx@FbAj@lB\\\\nE\\\\xBG`ACbADdAr@r@\\\\f@`@Tf@Fb@Af@Zx@bB\\\\j@h@J^b@Zh@NPb@Td@M^[f@@Xn@Np@Dt@VjBL^\\\\d@b@^l@fGB`AEdCXvBRbCFdA?bA[zC]j@CRDdAQ|CDbANz@Vt@f@xBXjEE`AMx@sBjEg@|@Sn@K`AQ|@Sx@[l@a@b@c@Vg@?c@N_@h@a@\\\\_@d@_@\\\\g@Rg@FUVCbAe@Rc@@e@Jc@\\\\_@d@[p@k@rBm@fB_@n@a@\\\\e@Lg@CiASi@Ci@Ve@Cg@GmAUg@OgAy@a@]c@O]h@Yp@UrAc@@a@[c@c@g@q@\",\r\n"
+				+ "			\"resource_state\": 2\r\n"
+				+ "		},\r\n"
+				+ "		\"trainer\": false,\r\n"
+				+ "		\"commute\": false,\r\n"
+				+ "		\"manual\": false,\r\n"
+				+ "		\"private\": false,\r\n"
+				+ "		\"visibility\": \"everyone\",\r\n"
+				+ "		\"flagged\": false,\r\n"
+				+ "		\"gear_id\": \"g6382672\",\r\n"
+				+ "		\"from_accepted_tag\": false,\r\n"
+				+ "		\"upload_id_str\": \"4486564059\",\r\n"
+				+ "		\"average_speed\": 3.317,\r\n"
+				+ "		\"max_speed\": 4.3,\r\n"
+				+ "		\"average_cadence\": 92.6,\r\n"
+				+ "		\"has_heartrate\": true,\r\n"
+				+ "		\"average_heartrate\": 129.5,\r\n"
+				+ "		\"max_heartrate\": 144.0,\r\n"
+				+ "		\"heartrate_opt_out\": false,\r\n"
+				+ "		\"display_hide_heartrate_option\": true,\r\n"
+				+ "		\"elev_high\": 55.1,\r\n"
+				+ "		\"elev_low\": 8.9,\r\n"
+				+ "		\"pr_count\": 0,\r\n"
+				+ "		\"total_photo_count\": 0,\r\n"
+				+ "		\"has_kudoed\": false\r\n"
+				+ "	}]");
 		
-		//.appendQueryParameter("client_id", "1234321")
-        //.appendQueryParameter("redirect_uri", "https://www.yourapp.com")
-        //.appendQueryParameter("response_type", "code")
-        //.appendQueryParameter("approval_prompt", "auto")
-        //.appendQueryParameter("scope", "activity:write,read")
-		 String[] authNames = new String[1];
-		    
-		 authNames[0] = "strava_oauth";
-		 defaultClient.updateParamsForAuth(authNames, queryParams, headerParams);
-		//Authentice user
-		AthletesApi apiInstance = new AthletesApi(defaultClient);
-
-		//AthletesApi apiInstance = new AthletesApi();
-		Long id = 13986969L; // Long | The identifier of the athlete. Must match the authenticated athlete.
-		try {
-		    ActivityStats result = apiInstance.getStats(id);
-		    System.out.println(result);
-
-		    apiInstance.setApiClient(defaultClient);
-		    //Login athlete
-		    DetailedAthlete detAthlete = apiInstance.getLoggedInAthlete();
-		    DetailedActivity detailedActivity =new DetailedActivity();
-
-
-		} catch (ApiException e) {
-		    System.err.println("Exception when calling AthletesApi#getStats");
-		    e.printStackTrace();
+		JSONArray array = new JSONArray(jsonArr);
+		System.out.println("Array:" + array);
+		for (int i = 0; i < array.length(); i++) {
+		  JSONObject row = array.getJSONObject(i);
+		  
+		  String name = row.getString("name");
+		  Long id = row.getLong("id");
+		  System.out.println("Name: " + name);
+		  System.out.println("ID: " + id);
 		}
-		
-		
-//		apiInstance = new ActivitiesApi();
-//		String name = "name_example"; // String | The name of the activity.
-//		String type = "type_example"; // String | Type of activity. For example - Run, Ride etc.
-//		OffsetDateTime startDateLocal = OffsetDateTime.now(); // OffsetDateTime | ISO 8601 formatted date time.
-//		Integer elapsedTime = 56; // Integer | In seconds.
-//		String description = "description_example"; // String | Description of the activity.
-//		Float distance = 3.4F; // Float | In meters.
-//		Integer trainer = 56; // Integer | Set to 1 to mark as a trainer activity.
-//		Integer commute = 56; // Integer | Set to 1 to mark as commute.
-//		try {
-//			DetailedActivity result = apiInstance.createActivity(name, type, startDateLocal, elapsedTime, description,
-//					distance, trainer, commute);
-//			System.out.println(result);
-//		} catch (ApiException e) {
-//			System.err.println("Exception when calling ActivitiesApi#createActivity");
-//			e.printStackTrace();
-//		}
 
+		
+		
 	}
 
 }
--- src/main/java/org/weewelchie/strava/stats/StravaServlet.java
+++ src/main/java/org/weewelchie/strava/stats/StravaServlet.java
@@ -15,18 +15,29 @@ import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.client.response.OAuthAuthzResponse;
 import org.apache.oltu.oauth2.client.response.OAuthJSONAccessTokenResponse;
 import org.apache.oltu.oauth2.client.response.OAuthResourceResponse;
+import org.apache.oltu.oauth2.common.OAuth.HttpMethod;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.types.GrantType;
-
-import io.swagger.client.auth.OAuth;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class StravaServlet extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public boolean authorize = true;
+	private static final String URL = "http://localhost:8888/strava-stats";
+	private static final String CLIENT_SECRET = "d906ca7e1a4cecc32a33714266d5baeaa6918e75";
+	private static final String CLIENT_ID = "48445";
+	private static final Double METRES_TO_MILES = 0.000621371;
+	private static final int MINUTES = 60;
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
 		if (authorize) {
 			try {
 				// Create the End User Authorization Request by
@@ -34,13 +45,13 @@ public class StravaServlet extends HttpServlet {
 						// providing the Strava authorization endpoint,
 						.authorizationLocation("https://www.strava.com/oauth/authorize")
 						// setting the Client ID of your registered application,
-						.setClientId("48445")
+						.setClientId(CLIENT_ID)
 						// setting response type to code,
 						.setResponseType("code")
 						// setting scope to view_private (optional),
-						.setScope("view_private")
+						.setScope("activity:read")
 						// setting the redirect URI back to the servlet.
-						.setRedirectURI("http://localhost:8080/strava-app").buildQueryMessage();
+						.setRedirectURI(URL).buildQueryMessage();
 				authorize = false;
 				/*
 				 * If scope is not public the user is redirected to Strava asking for
@@ -64,11 +75,11 @@ public class StravaServlet extends HttpServlet {
 						// setting grant type to authorization code,
 						.setGrantType(GrantType.AUTHORIZATION_CODE)
 						// setting the Client ID of your registered application,
-						.setClientId("48445")
+						.setClientId(CLIENT_ID)
 						// setting the Client secret of your registered application,
-						.setClientSecret("foobar")
+						.setClientSecret(CLIENT_SECRET)
 						// setting the redirect URI back to the servlet,
-						.setRedirectURI("http://localhost:8080/strava-app")
+						.setRedirectURI(URL)
 						// setting the previously requested oauth code.
 						.setCode(code).buildQueryMessage();
 
@@ -78,16 +89,79 @@ public class StravaServlet extends HttpServlet {
 						OAuthJSONAccessTokenResponse.class);
 				String accessToken = oAuthResponse.getAccessToken();
 
-				// Use the access token to query Strava API.
+				//Get Activity Details
+				//https://www.strava.com/api/v3/athlete/activities
 				OAuthClientRequest bearerClientRequest = new OAuthBearerClientRequest(
-						"https://www.strava.com/api/v3/athlete/clubs").setAccessToken(accessToken).buildQueryMessage();
+							"https://www.strava.com/api/v3/athlete/activities").setAccessToken(accessToken).buildQueryMessage();
 
 				// Get the response and print the body.
-				OAuthResourceResponse resourceResponse = oAuthClient.resource(bearerClientRequest, OAuth.HttpMethod.GET,
-						OAuthResourceResponse.class);
-				PrintWriter out = resp.getWriter();
-				out.println(resourceResponse.getBody());
-
+				OAuthResourceResponse resourceResponse = oAuthClient.resource(bearerClientRequest, HttpMethod.GET,
+							OAuthResourceResponse.class);
+				
+				String responseBody = resourceResponse.getBody();
+				String resType=resourceResponse.getContentType();
+				
+				
+				out.println("Activity Details");
+				out.flush();
+	
+				out.println("<html><head>Heading</head><body><table style='width:100%'>");
+				
+				JSONArray array = new JSONArray(responseBody);
+				JSONArray outputArray = new JSONArray();
+				
+				for (int i = 0; i < array.length(); i++) {
+				  JSONObject row = array.getJSONObject(i);
+				  
+				  String name = row.getString("name");
+				  Long id = row.getLong("id");
+				  JSONObject obj = new JSONObject();
+				  obj.append("name", name);
+				  obj.append("id", id);
+			
+				  //Now get the detail activity here and get the description
+				  bearerClientRequest = new OAuthBearerClientRequest(
+							"https://www.strava.com/api/v3//activities/" + id).setAccessToken(accessToken).buildQueryMessage();
+			
+				  // Get the response and print the body.
+				  resourceResponse = oAuthClient.resource(bearerClientRequest, HttpMethod.GET,
+							OAuthResourceResponse.class);
+				
+				  responseBody = resourceResponse.getBody();
+				  JSONObject jsonObj = new JSONObject(responseBody);
+				  
+				  String desc = jsonObj.getString("description");
+				  obj.append("description", desc);
+				  
+				  Double distance = jsonObj.getDouble("distance");
+				  distance = distance * METRES_TO_MILES; //0.000621371;
+				  obj.append("distance(miles)", distance);
+				  
+				  Double movingTime = jsonObj.getDouble("moving_time");
+				  Double minutes = movingTime/MINUTES; ///60;
+				  obj.append("moving_time(mins)", minutes);
+				  
+				  String type = jsonObj.getString("type");
+				  obj.append("type", type);
+				  out.println("<tr>");
+				  out.println("<td>");
+				  out.println(obj);
+				  out.println("</td>");
+				  out.println("</tr>");				  
+				  outputArray.put(obj);
+				  
+				  //out.println("OutputArray size: " + outputArray.length());
+				  System.out.println("OutputArray size: " + outputArray.length());
+				  
+				  out.flush();
+				}
+				out.println("</table></body></html>");
+				
+				System.out.println("******************** End of the loop here **********************");
+				System.out.println("==============================================================================");
+				System.out.println(outputArray);
+				
+				
 				authorize = true;
 			} catch (OAuthSystemException e) {
 				e.printStackTrace();
